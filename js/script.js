cb74c43d4098035218486f1f1be8ddf01cd5b545
sujon3537 <119605414+sujon3537@users.noreply.github.com>
2025-06-15 02:01:43 +0600

    modal

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
-const url = "https://www.themealdb.com/api/json/v1/1/search.php?s=";
-
 async function getRecipes() {
+  const url = "https://www.themealdb.com/api/json/v1/1/search.php?s=";
+
   try {
     const response = await fetch(url);
     const data = await response.json();
@@ -17,15 +17,42 @@ async function getRecipes() {
             <p>
               ${recipe.strInstructions}
             </p>
-            <button>View details</button>
+            <button class="detail-btn" data-meal-id=${recipe.idMeal} onclick="my_modal_1.showModal()">View details ${recipe.idMeal}</button>
           </div>
         </div>`
       )
       .join(" ");
+
+    const btnList = document.querySelectorAll(".detail-btn");
+    return btnList;
   } catch (error) {
     const loader = document.querySelector(".loader");
     loader.innerHTML = `<img src="../images/not found.png" alt="not found" width="450"/>`;
   }
 }
 
-getRecipes();
+getRecipes().then((buttons) => {
+  buttons.forEach((button) => {
+    let id = button.getAttribute("data-meal-id");
+    button.addEventListener("click", () => getDetails(id));
+  });
+});
+
+// async function getDetails(recipeID) {
+//   const url = `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${recipeID}`;
+//   document.querySelector(".modal").innerHTML = "";
+
+//   const response = await fetch(url);
+//   const data = await response.json();
+//   const recipe = data.meals[0];
+//   // console.log(recipe.strMeal, recipe.strInstructions, recipe.strMealThumb);
+//   document.querySelector(".modal").innerHTML = `<div class="modal-box">
+//         <h3 class="text-lg font-bold">${recipe.strMeal}</h3>
+//         <p class="py-4">Press ESC key or click the button below to close</p>
+//         <div class="modal-action">
+//           <form method="dialog">
+//             <button class="btn" id="close-btn">Close</button>
+//           </form>
+//         </div>
+//       </div>`;
+// }
